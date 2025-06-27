667f28270ca01364732101f857b3ad2f1e14c274
sweethuman <gheo@sweethuman.tech>
2025-06-27 15:27:48 +0300

    fix: change base domain to youtrack.cloud

--- contents/issue_page_subtasks.tsx
+++ contents/issue_page_subtasks.tsx
@@ -1,55 +1,62 @@
-import type {PlasmoCSConfig, PlasmoGetInlineAnchorList} from "plasmo"
+import type { PlasmoCSConfig, PlasmoGetInlineAnchorList } from "plasmo"
 import styleText from "data-text:~/contents/statuses.css"
 
 import "~/contents/statuses.css"
-import {get_issue_state_class, fields} from "~youtrack/issue";
-import {useStorage} from "~node_modules/@plasmohq/storage/dist/hook";
+import { get_issue_state_class, fields } from "~youtrack/issue"
+import { useStorage } from "~node_modules/@plasmohq/storage/dist/hook"
 
 export const config: PlasmoCSConfig = {
-    matches: ["https://chainstack.myjetbrains.com/*"]
+  matches: ["https://*.youtrack.cloud/*"]
 }
 
 export const getInlineAnchorList: PlasmoGetInlineAnchorList = async () =>
-    document.querySelectorAll(`.yt-issue-links-list div.yt-issue .yt-issue-links-list-content__summary`)
+  document.querySelectorAll(
+    `.yt-issue-links-list div.yt-issue .yt-issue-links-list-content__summary`
+  )
 
 export const getStyle = () => {
-    const style = document.createElement("style")
-    style.textContent = styleText
-    return style
+  const style = document.createElement("style")
+  style.textContent = styleText
+  return style
 }
 
-const IssuePageSubtask = ({anchor}) => {
-    let issueId = anchor.element.querySelector(`.js-issue-id`).textContent.trim();
-    const [access_token] = useStorage<string>("youtrack_token")
-    const [base_url] = useStorage<string>("base_url")
+const IssuePageSubtask = ({ anchor }) => {
+  let issueId = anchor.element.querySelector(`.js-issue-id`).textContent.trim()
+  const [access_token] = useStorage<string>("youtrack_token")
+  const [base_url] = useStorage<string>("base_url")
 
-    if(!issueId) return;
-    if(!base_url) return;
-    if(!access_token) return;
+  if (!issueId) return
+  if (!base_url) return
+  if (!access_token) return
 
-    let yturl = base_url.endsWith('/') ? base_url.slice(0, -1) : base_url;
+  let yturl = base_url.endsWith("/") ? base_url.slice(0, -1) : base_url
 
-    fetch(yturl + '/youtrack/api/issues/' + issueId + '?$top=-1&$topLinks=0&fields=' + fields, {
-        headers: {
-            'Authorization': 'Bearer ' + access_token,
-            'Content-Type': 'application/json',
-        }
+  fetch(
+    yturl +
+      "/youtrack/api/issues/" +
+      issueId +
+      "?$top=-1&$topLinks=0&fields=" +
+      fields,
+    {
+      headers: {
+        Authorization: "Bearer " + access_token,
+        "Content-Type": "application/json"
+      }
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let state = data.fields.find((obj) => {
+        return obj.name === "State"
+      })
+
+      let style = get_issue_state_class(state.value.name)
+
+      let span = document.createElement("span")
+      span.className = "ch-badge " + style
+      span.textContent = state.value.name
+      anchor.element.prepend(span)
     })
-        .then((response) => response.json())
-        .then((data) => {
-
-            let state = data.fields.find(obj => {
-                return obj.name === 'State'
-            });
-
-
-            let style = get_issue_state_class(state.value.name);
-
-            let span = document.createElement("span");
-            span.className = 'ch-badge ' + style;
-            span.textContent = state.value.name
-            anchor.element.prepend(span);
-        });
 }
 
-export default IssuePageSubtask;
+export default IssuePageSubtask
--- popup.tsx
+++ popup.tsx
@@ -1,31 +1,41 @@
 import { useStorage } from "@plasmohq/storage/hook"
 
 function IndexPopup() {
+  const [youtrackToken, setYoutrackToken] = useStorage("youtrack_token", "")
+  const [baseURL, setBaseURL] = useStorage("base_url", "")
 
-    const [youtrackToken, setYoutrackToken] = useStorage("youtrack_token", "")
-    const [baseURL, setBaseURL] = useStorage("base_url", "")
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16,
+        minHeight: "30rem",
+        minWidth: "20rem"
+      }}>
+      <h2>
+        Youtrack plugin{" "}
+        <a
+          href="https://chainstack.youtrack.cloud/youtrack/users/me?tab=account-security"
+          target="_blank">
+          Get Your Token here
+        </a>{" "}
+      </h2>
 
-    return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                padding: 16
-            }}>
-            <h2>
-                Youtrack plugin {" "}
-                <a href="https://chainstack.myjetbrains.com/youtrack/users/me?tab=account-security" target="_blank">
-                    Get Your Token here
-                </a>{" "}
-
-            </h2>
-
-            <label htmlFor="youtrack_token">Youtrack base URL</label>
-            <input onChange={(e) => setBaseURL(e.target.value)} id="base_url" value={baseURL} />
-            <label htmlFor="youtrack_token">Insert your youtrack token</label>
-            <input onChange={(e) => setYoutrackToken(e.target.value)} id="youtrack_token" value={youtrackToken} />
-        </div>
-    )
+      <label htmlFor="youtrack_token">Youtrack base URL</label>
+      <input
+        onChange={(e) => setBaseURL(e.target.value)}
+        id="base_url"
+        value={baseURL}
+      />
+      <label htmlFor="youtrack_token">Insert your youtrack token</label>
+      <input
+        onChange={(e) => setYoutrackToken(e.target.value)}
+        id="youtrack_token"
+        value={youtrackToken}
+      />
+    </div>
+  )
 }
 
 export default IndexPopup
